d1e5cdbace4277e6f4ceef1ea8067c56036a0728
Lucas Silva <lucsamds@gmail.com>
2023-12-03 22:59:46 -0300

    finishing watchlist page

--- src/app/components/watchlist/watchlist.component.ts
+++ src/app/components/watchlist/watchlist.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/models/movie.model';
+import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
   selector: 'app-watchlist',
   templateUrl: './watchlist.component.html',
   styleUrls: ['./watchlist.component.css']
 })
-export class WatchlistComponent {
+export class WatchlistComponent implements OnInit{
+  watchlist!: number[];
+  movies!: Movie[];
 
+  constructor(private movieService: MovieService){}
+
+  ngOnInit(): void {
+      this.getWatchlist()
+      this.movieService.getAllMovies().subscribe({
+        next: (data) => {
+          this.movies = data;
+          this.filterMovies();
+        },
+        error: (error) => console.error('Error found', error)
+      })
+  }
+
+  /** Recieves user's watchlist  */
+  getWatchlist(): void{
+    let user_watchlist = localStorage.getItem('watchlist');
+    this.watchlist = user_watchlist? JSON.parse(user_watchlist) : [];
+  }
+
+  /** Filter movies on watchlist */
+  filterMovies(): void{
+    this.movies = this.movies.filter((movie) => this.watchlist.includes(movie.id));
+  }
 }
